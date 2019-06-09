02830615dd457539e3521796e4db61ef03186311
YaCpotato <yasshisshy@gmail.com>
2019-06-09 21:34:29 +0900

    conv32,64NoNo

--- train1000-master/untitled4.py
+++ train1000-master/untitled4.py
@@ -21,10 +21,10 @@ import train1000
 
 def model():
     model = Sequential()
-    model.add(Conv2D(128, (3, 3), padding="same", input_shape=X_train.shape[1:]))
+    model.add(Conv2D(32, (3, 3), padding="same", input_shape=X_train.shape[1:]))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.5))
+    model.add(Dropout(0.7))
     model.add(Flatten())
     model.add(Dense(512))
     model.add(Activation('relu'))
@@ -33,6 +33,8 @@ def model():
     model.add(Activation('softmax'))
     model.compile(loss='categorical_crossentropy', optimizer=Adam(), metrics=['categorical_crossentropy', 'accuracy'])
     model.summary()
+    f = open('trainwith1000result.txt','a')
+    f.close()
     return model
 
 
@@ -42,13 +44,19 @@ if __name__ == '__main__':
     (X_train, Y_train), (X_test, Y_test) = train1000.cifar10()
     nb_classes = 10
     model = model()
+    t = time.time()
     result = model.fit(X_train, Y_train, batch_size=batch_size, epochs=epochs, validation_split=0.1,verbose=1)
+    total = time.time() - t
+    print(total)
+    f = open('trainwith1000result.txt','a')
     print('train data:')
     eva=model.evaluate(X_train,Y_train,verbose=1)
     for i in range(1,len(model.metrics_names)):
         print( model.metrics_names[i] + ' : ', eva[i] )
     print()
     print('test data:')
+    f.write('test data')
     eva = model.evaluate(X_test,Y_test,verbose=1)
     for i in range(1,len(model.metrics_names)):
         print( model.metrics_names[i] + ' : ', eva[i] )
+    f.close()
